8dcc570d1285d76d813bee5f574b369dfd3d61d9
fco.mosan.ia <fco.mosan.ia@gmail.com>
2024-11-28 17:55:01 +0000

    creacion de navbar

--- app/(general)/layout.tsx
+++ app/(general)/layout.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from "@/components";
 
 export default function GeneralLayout({
   children,
@@ -5,10 +6,13 @@ export default function GeneralLayout({
   children: React.ReactNode;
 }) {
   return (
+    <>
+    <Navbar />
     <main className="flex flex-col items-center p-24">
       <span>Auto Fenix</span>
       {children}
     </main>
+    </>
   );
 }
 
